d42a2de2dca70bf9ec9ff37771dc846627709e7b
Gonzausado <venturagonzalo7@gmail.com>
2024-12-03 17:48:04 -0300

    checkpoint lindoo

--- Assets/Scripts/Misc/Checkpoint.cs
+++ Assets/Scripts/Misc/Checkpoint.cs
@@ -8,6 +8,7 @@ public class Checkpoint : MonoBehaviour
     public int checkpointID;              // Unique ID for each checkpoint
     public Transform respawnPoint;
     public bool isTrigger;
+    public ParticleSystem chkpntParticles;
     private void OnTriggerEnter(Collider other)
     {
         isTrigger = true;
@@ -15,6 +16,7 @@ public class Checkpoint : MonoBehaviour
         if (isTrigger)
         {
             SaveCheckpoint();
+            PlayParticles();
             Debug.Log("Checkpoint: " + checkpointID);
         }
     }
@@ -27,5 +29,13 @@ public class Checkpoint : MonoBehaviour
         PlayerPrefs.SetFloat("RespawnY", respawnPoint.position.y);
         PlayerPrefs.SetFloat("RespawnZ", respawnPoint.position.z);
         PlayerPrefs.Save();
+    }
+
+    private void PlayParticles()
+    {
+        if (isTrigger)
+        {
+            chkpntParticles.Play();
+        }
     }
 }
